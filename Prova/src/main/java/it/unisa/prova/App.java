1a159de0c217f1dceb8e836fc1a1585f5a6214ee
skane <skane@mbp-di-mdr>
2021-11-16 17:22:04 +0100

    ho aggiunto la print

--- Prova/src/main/java/it/unisa/prova/App.java
+++ Prova/src/main/java/it/unisa/prova/App.java
@@ -32,7 +32,7 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        System.out.println("ciao");
+        System.out.println("");
         launch();
     }
 
